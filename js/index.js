c00080d18fdb4010d33208926bc6082714178097
Federico-creator <fbornico@udesa.edu.ar>
2020-11-14 14:51:23 -0300

    final 2

--- js/index.js
+++ js/index.js
@@ -1,46 +1,62 @@
 window.addEventListener("load",function(){
     alert ("bienvenidos al Tops, somos el uno, somos el Tops")
-    let pelicula = document.querySelector(".peliculas")
+    //fetch de peliculas populares
     fetch("https://api.themoviedb.org/3/movie/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1")
     .then(function(respuesta){
         return respuesta.json()
     })
-    .then(function(pelis){
-        pelis.results.forEach(peliculas => {
-            let articulo = document.createElement("article")
-            articulo.innerHTML += `<div class= "pelicula"> <img src="https://image.tmdb.org/t/p/w500${peliculas.poster_path}" alt= "${peliculas.title}"> <a href="serieDetail.html" class="links">${peliculas.title}</a> </div>`
-            pelicula.append(articulo) 
+    .then(function(datos){
+        let slider = document.getElementById("peliculasPopulares")
+        datos.results.forEach(d => {
+            slider.innerHTML += `<li class="pelicula"><a href="movieDetail.html?id=${d.id}"> <img src="https://image.tmdb.org/t/p/w500/${d.poster_path}"></a></li>`
         });
+        
     })
     .catch(function(error){
         console.log(error);
     })
-    let serie = document.querySelector(".series")
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1")
+    // fetch de peliculas estrenos
+    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=2")
     .then(function(respuesta){
         return respuesta.json()
     })
-    .then(function(seris){
-        seris.results.forEach(series => {
-            let articulo = document.createElement("article")
-            articulo.innerHTML += `<div class= "serie"> <img src="https://image.tmdb.org/t/p/w500${series.poster_path}" alt= "${series.title}"> <a href="serieDetail.html" class="links">${series.title}</a> </div>`
-            serie.append(articulo) 
+    .then(function(datos){
+        let slider = document.getElementById("peliculasEstreno")
+        datos.results.forEach(d => {
+            slider.innerHTML += `<li class="pelicula"><a href="movieDetail.html?id=${d.id}"> <img src="https://image.tmdb.org/t/p/w500/${d.poster_path}"></a></li>`
         });
+        
     })
     .catch(function(error){
         console.log(error);
     })
-    let actores = document.querySelector(".actores")
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1")
+
+    //fetch de series populares
+    fetch("https://api.themoviedb.org/3/tv/popular?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=1")
     .then(function(respuesta){
         return respuesta.json()
     })
-    .then(function(act){
-        act.results.forEach(actor => {
-            let articulo = document.createElement("article")
-            articulo.innerHTML += `<div class= "actor"> <img src="https://image.tmdb.org/t/p/w500${actor.poster_path}" alt= "${actor.title}"> <a href="serieDetail.html" class="links">${actor.title}</a> </div>`
-            actores.append(articulo)
+    .then(function(datos){
+        let slider = document.getElementById("seriesPopulares")
+        datos.results.forEach(d => {
+            slider.innerHTML += `<li class="pelicula"><a href="movieDetail.html?id=${d.id}"> <img src="https://image.tmdb.org/t/p/w500/${d.poster_path}"></a></li>`
         });
+        
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+    //fetch de series Estreno
+    fetch("https://api.themoviedb.org/3/tv/on_the_air?api_key=0d278db4bda20f994d6bf90837dc480e&language=en-US&page=2")
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(datos){
+        let slider = document.getElementById("seriesEmision")
+        datos.results.forEach(d => {
+            slider.innerHTML += `<li class="pelicula"><a href="movieDetail.html?id=${d.id}"> <img src="https://image.tmdb.org/t/p/w500/${d.poster_path}"></a></li>`
+        });
+        
     })
     .catch(function(error){
         console.log(error);
