cebfeec074f0dd05f16f01164c4d379008a9300e
Federico-creator <fbornico@udesa.edu.ar>
2020-11-27 15:20:04 -0300

    final final

--- js/descubrir.js
+++ js/descubrir.js
@@ -3,11 +3,8 @@ window.addEventListener("load",function(){
   function aleatorio() {
     return Math.floor(Math.random() * 9542);
   }
-  buscarRandom.innerHTML = `<a href="movieDetail.html?id=${aleatorio()}">descubrir pelicula</a>`
+  buscarRandom.innerHTML = `<a href="movieDetail.html?id=${aleatorio()}">#Descubrir Película</a>`
 
   var buscarRandom = document.querySelector(".descubrirSerie")
-  function aleatorio() {
-    return Math.floor(Math.random() * 9542);
-  }
-  buscarRandom.innerHTML = `<a href="serieDetail.html?id=${aleatorio()}">descubrir Serie</a>`
+  buscarRandom.innerHTML = `<a href="serieDetail.html?id=${aleatorio()}">#Descubrir Serie</a>`
 })
